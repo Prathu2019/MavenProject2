625c4f71239baadf952e3c80b8f147ee7962befb
LENOVO <LENOVO@DEVIL>
2024-01-19 12:24:34 +0530

    maven

---
+++ MavenProject2/src/test/java/testPackage/Login.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Login {
+
+	@Test
+	
+	public void Username(){
+		Reporter.log("Username",true);
+		
+	}
+}
